223edaeb7838b039071eacb4e6f59fb01ab39a5a
ismailsemih <59211902+ismailsemih@users.noreply.github.com>
2024-08-23 20:31:19 +0300

    update authContext

--- src/services/authContext.tsx
+++ src/services/authContext.tsx
@@ -1,27 +1,22 @@
 import React, { createContext, useState, useContext, useEffect, ReactNode } from 'react';
 import { onAuthStateChanged, User } from 'firebase/auth';
-import { auth } from '../../firebase.config'; // Firebase config dosyanızın yolunu doğru şekilde belirtin
+import { auth } from '../../firebase.config';
 
-// AuthContextType interface'ini tanımlayalım
 interface AuthContextType {
   user: User | null;
   loading: boolean;
 }
 
-// AuthContext'i oluşturalım
 const AuthContext = createContext<AuthContextType>({ user: null, loading: true });
 
-// Custom hook for using the auth context
 export function useAuth(): AuthContextType {
   return useContext(AuthContext);
 }
 
-// AuthProvider props interface'i
 interface AuthProviderProps {
   children: ReactNode;
 }
 
-// AuthProvider component'i
 export function AuthProvider({ children }: AuthProviderProps): JSX.Element {
   const [authState, setAuthState] = useState<AuthContextType>({ user: null, loading: true });
 
@@ -30,7 +25,7 @@ export function AuthProvider({ children }: AuthProviderProps): JSX.Element {
       setAuthState({ user: currentUser, loading: false });
     });
 
-    // Cleanup subscription on unmount
+    
     return () => unsubscribe();
   }, []);
 
