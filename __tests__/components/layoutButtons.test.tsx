24adc0dd6051801b9f20d44472ff933e2c7ea7e3
ismailsemih <59211902+ismailsemih@users.noreply.github.com>
2024-08-24 14:42:19 +0300

    config update

--- __tests__/components/layoutButtons.test.tsx
+++ __tests__/components/layoutButtons.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react-native';
 import Button from '../../src/components/common/layoutButtons';
 import React from 'react';
 
-let children: React.ReactNode = "Click me"
+const children: React.ReactNode = "Click me"
 test('renders a button with correct text', () => {
   render(<Button onPress={() => console.log("pressed")}>{children} </Button> );
   const button = screen.getByText('Click me');
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,14 +5,14 @@ import pluginReact from "eslint-plugin-react";
 
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
-  {
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     rules: {
       'no-unused-vars': 'off',
       'react/prop-types': 'off',
-    }
-  },
+      'no-undef':'off',
+    },
+   },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
