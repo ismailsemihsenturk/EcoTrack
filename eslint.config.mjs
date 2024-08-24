cad3a452b303b118d2a54e27db9c4544d88efeeb
ismailsemih <59211902+ismailsemih@users.noreply.github.com>
2024-08-24 14:56:07 +0300

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,25 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+      },
+    },
     rules: {
       'no-unused-vars': 'off',
       'react/prop-types': 'off',
-      'react/jsx-no-undef':'off',
+      'react/jsx-no-undef': 'off',
     },
-   },
-  { languageOptions: { globals: globals.browser } },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
